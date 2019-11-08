826c38e6021ba4db9b612a5b9beaa60d3ee17b68
elfraim <elfraiman@gmail.com>
2019-11-08 10:57:23 +0100

    feat(log-rocket): log

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import Welcome from './Welcome.svelte';
+import LogRocket from 'logrocket';
+
+// Initialise logRocket
+LogRocket.init('q9gyyz/make-shots-great-again');
 
 const app = new Welcome({
 	target: document.body
